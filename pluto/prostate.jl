0667761da9668e6e2880bbd5fee190987f069b2b
Nikos Ignatiadis <nikos.ignatiadis01@gmail.com>
2021-06-24 18:34:51 -0700

    minor edits to prostate notebook

--- pluto/prostate.jl
+++ pluto/prostate.jl
@@ -1,5 +1,5 @@
 ### A Pluto.jl notebook ###
-# v0.12.21
+# v0.14.8
 
 using Markdown
 using InteractiveUtils
@@ -25,8 +25,7 @@ md"""
 # ╔═╡ 473d8a28-4bf4-11eb-0151-8b3f3d286eda
 begin
 	pgfplotsx()
-	deleteat!(PGFPlotsX.CUSTOM_PREAMBLE,
-			  Base.OneTo(length(PGFPlotsX.CUSTOM_PREAMBLE)))
+	empty!(PGFPlotsX.CUSTOM_PREAMBLE)
 	push!(PGFPlotsX.CUSTOM_PREAMBLE, raw"\usepackage{amssymb}")
 	push!(PGFPlotsX.CUSTOM_PREAMBLE, raw"\newcommand{\PP}[2][]{\mathbb{P}_{#1}\left[#2\right]}")
 	push!(PGFPlotsX.CUSTOM_PREAMBLE, raw"\newcommand{\EE}[2][]{\mathbb{E}_{#1}\left[#2\right]}")
@@ -38,6 +37,7 @@ theme(:default;
       foreground_color_legend = :transparent,
       grid=nothing,
       frame=:box,
+      legendfonthalign = :left,
       thickness_scaling=1.3)
 
 # ╔═╡ acfc4fc0-4bf4-11eb-32dc-dfe1652ac006
@@ -57,11 +57,11 @@ dkw_floc = DvoretzkyKieferWolfowitz(0.05)
 fitted_dkw = fit(dkw_floc, Zs)
 
 # ╔═╡ 246264dc-4bf5-11eb-2f9d-b5c7d137a9b3
-dkw_plot = plot(fitted_dkw, subsample=300, label="DKW band",
+dkw_plot = plot(fitted_dkw, label="DKW band",
      xlab=L"z", ylab=L"\widehat{F}_n(z)",  size=(380,280))
 
 # ╔═╡ 4dd1f99a-4bf5-11eb-3691-9db09ab324df
-savefig(dkw_plot, "prostate_dkw_band.tikz")
+# savefig(dkw_plot, "prostate_dkw_band.tikz")
 
 # ╔═╡ 362effcc-4bf5-11eb-1d84-3133de3f4825
 md"### KDE-F-Localization"
@@ -74,18 +74,19 @@ fitted_infty_floc = fit(infty_floc, Zs)
 
 # ╔═╡ 69e87366-4bf5-11eb-3fbb-8f7bf325d366
 prostate_kde_plot = begin
-	prostate_marginal_plot = histogram(response.(Zs), bins=50, normalize=true,
+	prostate_marginal_plot = histogram([response(Z) for Z in Zs],
+		  bins=50, normalize=true,
           label="Histogram", fillalpha=0.4, linealpha=0.4, fillcolor=:lightgray,
           size=(380,280), xlims=(-4.5,4.5))
 	plot!(prostate_marginal_plot, fitted_infty_floc,
        	  label="KDE band", xlims=(-4.5,4.5),
           yguide=L"\widehat{f}_n(z)", xguide=L"z")
-	plot!([-3.0;3.0], seriestype=:vline,
+	plot!(prostate_marginal_plot, [-3.0;3.0], seriestype=:vline,
 		  linestyle=:dot, label=nothing, color=:lightgrey)
 end
 
 # ╔═╡ 750e2af0-4bf6-11eb-3374-1f224647d4e5
-savefig("prostate_kde_band.tikz")
+# savefig("prostate_kde_band.tikz")
 
 # ╔═╡ aa99cc74-4bf6-11eb-2e3f-81de213db00d
  md"## Confidence intervals"
@@ -118,7 +119,7 @@ floc_method_kde_locmix = FLocalizationInterval(flocalization = infty_floc,
 
 # ╔═╡ 04c72ec6-4bf7-11eb-3265-5f69502b9f0b
 
-lam_kde_locmix = Empirikos.AMARI(
+amari_kde_locmix = Empirikos.AMARI(
 						convexclass = gcal_locmix,
 						flocalization = (@set infty_floc.α=0.01),
                         discretizer=discr,
@@ -137,7 +138,7 @@ floc_method_kde_scalemix = FLocalizationInterval(flocalization = infty_floc,
 
 
 # ╔═╡ 474bdc10-4bf7-11eb-0099-cdf3150001a2
-lam_kde_scalemix = Empirikos.AMARI(
+amari_kde_scalemix = Empirikos.AMARI(
 						convexclass = gcal_scalemix,
 						flocalization = (@set infty_floc.α=0.01),
                         discretizer=discr,
@@ -161,25 +162,25 @@ postmean_ci_dkw_locmix = confint.(floc_method_dkw_locmix, postmean_targets, Zs)
 postmean_ci_kde_locmix = confint.(floc_method_kde_locmix, postmean_targets, Zs)
 
 # ╔═╡ 89a047c2-4bf7-11eb-3c79-15fe2fcdcb1c
-postmean_ci_lam_locmix = confint.(lam_kde_locmix, postmean_targets, Zs)
+postmean_ci_amari_locmix = confint.(amari_kde_locmix, postmean_targets, Zs)
 
 # ╔═╡ a262fe94-4bf7-11eb-1153-6f76128cbcd0
 postmean_locmix_plot = begin
 	postmean_locmix_plot = plot(ts, postmean_ci_kde_locmix, label="KDE-F-Loc",
-		fillcolor=:darkorange, fillalpha=0.5, ylim=(-2.55,2.55))
+		fillcolor=:darkorange, fillalpha=0.5, ylim=(-2.55,2.55),
+        xguide = L"z", yguide=L"\EE{\mu \mid Z=z}",
+        size=(380,280))
 	plot!(postmean_locmix_plot, ts, postmean_ci_dkw_locmix, label="DKW-F-Loc",
 		show_ribbon=false, alpha=0.9, color=:black)
-	plot!(postmean_locmix_plot, ts, postmean_ci_lam_locmix, label="Amari",
+	plot!(postmean_locmix_plot, ts, postmean_ci_amari_locmix, label="AMARI",
 		show_ribbon=true, fillcolor=:blue, fillalpha=0.4)
 	plot!(postmean_locmix_plot, [-3.0;3.0], [-3.0; 3.0], seriestype=:line,
 		linestyle=:dot, label=nothing, color=:lightgrey)
-	plot!(postmean_locmix_plot, xlabel = L"z",
-		ylabel=L"\EE{\mu \mid Z=z}", size=(380,280))
 end
 
 
 # ╔═╡ 622669fa-4bf8-11eb-08fd-a3bd67230729
-savefig(postmean_locmix_plot, "prostate_locmix_postmean.tikz")
+# savefig(postmean_locmix_plot, "prostate_locmix_postmean.tikz")
 
 # ╔═╡ 6930b1ae-4bf8-11eb-23c7-e5be40b7a283
 postmean_ci_dkw_scalemix = confint.(floc_method_dkw_scalemix, postmean_targets, Zs)
@@ -188,24 +189,25 @@ postmean_ci_dkw_scalemix = confint.(floc_method_dkw_scalemix, postmean_targets, 
 postmean_ci_kde_scalemix = confint.(floc_method_kde_scalemix, postmean_targets, Zs)
 
 # ╔═╡ 7c308bb4-4bf8-11eb-249a-0f6d933a530c
-postmean_ci_lam_scalemix = confint.(lam_kde_scalemix, postmean_targets, Zs)
+postmean_ci_amari_scalemix = confint.(amari_kde_scalemix, postmean_targets, Zs)
 
 # ╔═╡ 8abdac16-4bf8-11eb-3b0d-a73d7b28bb7c
 postmean_scalemix_plot = begin
 	postmean_scalemix_plot = plot(ts, postmean_ci_kde_scalemix,
-		label="KDE-F-Loc", fillcolor=:darkorange, fillalpha=0.5, ylim=(-2.55,2.55))
+		label="KDE-F-Loc", fillcolor=:darkorange, fillalpha=0.5, ylim=(-2.55,2.55),
+        xguide = L"z",
+		yguide=L"\EE{\mu \mid Z=z}",
+        size=(380,280))
 	plot!(postmean_scalemix_plot, ts, postmean_ci_dkw_scalemix,
 		label="DKW-F-Loc",show_ribbon=false, alpha=0.9, color=:black)
-	plot!(postmean_scalemix_plot, ts, postmean_ci_lam_scalemix,
-		label="Amari",show_ribbon=true, fillcolor=:blue, fillalpha=0.4)
+	plot!(postmean_scalemix_plot, ts, postmean_ci_amari_scalemix,
+		label="AMARI",show_ribbon=true, fillcolor=:blue, fillalpha=0.4)
 	plot!(postmean_scalemix_plot, [-3.0;3.0], [-3.0; 3.0], seriestype=:line,
 		linestyle=:dot, label=nothing, color=:lightgrey)
-	plot!(postmean_scalemix_plot, xlabel = L"z",
-		ylabel=L"\EE{\mu \mid Z=z}", size=(380,280))
 end
 
 # ╔═╡ eb851e8a-4bf8-11eb-353a-e5c394ae5294
-savefig(postmean_scalemix_plot, "prostate_scalemix_postmean.tikz")
+# savefig(postmean_scalemix_plot, "prostate_scalemix_postmean.tikz")
 
 # ╔═╡ 1e0db04c-4bf9-11eb-1bde-9dfa4f81f279
 lfsr_ci_dkw_locmix = confint.(floc_method_dkw_locmix, lfsrs, Zs)
@@ -214,24 +216,24 @@ lfsr_ci_dkw_locmix = confint.(floc_method_dkw_locmix, lfsrs, Zs)
 lfsr_ci_kde_locmix = confint.(floc_method_kde_locmix, lfsrs, Zs)
 
 # ╔═╡ 2eba35aa-4bf9-11eb-1bf0-0be32d4bff85
-lfsr_ci_lam_locmix = confint.(lam_kde_locmix, lfsrs, Zs)
+lfsr_ci_amari_locmix = confint.(amari_kde_locmix, lfsrs, Zs)
 
 # ╔═╡ a30dce02-4bfa-11eb-1197-5b69f20af457
 lfsr_locmix_plot = begin
 	lfsr_locmix_plot = plot([-3;3], [0.5; 0.5], seriestype=:line,
-		linestyle=:dot, label=nothing, color=:lightgrey)
+		linestyle=:dot, label=nothing, color=:lightgrey,
+        xguide = L"z",
+		yguide=L"\PP{\mu \geq 0 \mid Z=z}", size=(380,280))
 	plot!(lfsr_locmix_plot, ts, lfsr_ci_kde_locmix,
 		label="KDE-F-Loc", fillcolor=:darkorange, fillalpha=0.5, ylim=(0,1))
 	plot!(lfsr_locmix_plot, ts, lfsr_ci_dkw_locmix,
 		label="DKW-F-Loc",show_ribbon=false, alpha=0.9, color=:black)
-	plot!(lfsr_locmix_plot, ts, lfsr_ci_lam_locmix,
-		label="Amari",show_ribbon=true, fillcolor=:blue, fillalpha=0.4)
-	plot!(lfsr_locmix_plot, xlabel = L"z",
-		ylabel=L"\PP{\mu \geq 0 \mid Z=z}", size=(380,280))
+	plot!(lfsr_locmix_plot, ts, lfsr_ci_amari_locmix,
+		label="AMARI",show_ribbon=true, fillcolor=:blue, fillalpha=0.4)
 end
 
 # ╔═╡ 716b8220-4bf9-11eb-1a28-33dbf08e138c
-savefig(lfsr_locmix_plot, "prostate_locmix_lfsr.tikz")
+# savefig(lfsr_locmix_plot, "prostate_locmix_lfsr.tikz")
 
 # ╔═╡ 9afa4b4a-4bf9-11eb-2a16-531b86e6b9a9
 lfsr_ci_dkw_scalemix = confint.(floc_method_dkw_scalemix, lfsrs, Zs)
@@ -240,25 +242,25 @@ lfsr_ci_dkw_scalemix = confint.(floc_method_dkw_scalemix, lfsrs, Zs)
 lfsr_ci_kde_scalemix = confint.(floc_method_kde_scalemix, lfsrs, Zs)
 
 # ╔═╡ a1ba1e12-4bf9-11eb-00c9-496602f4eb5c
-lfsr_ci_lam_scalemix = confint.(lam_kde_scalemix, lfsrs, Zs)
+lfsr_ci_amari_scalemix = confint.(amari_kde_scalemix, lfsrs, Zs)
 
 # ╔═╡ f2414706-4bfa-11eb-1654-9bdcc7418db8
 lfsr_scalemix_plot = begin
 	lfsr_scalemix_plot = plot([-3;3], [0.5; 0.5], seriestype=:line,
-		linestyle=:dot, label=nothing, color=:lightgrey)
+		linestyle=:dot, label=nothing, color=:lightgrey,
+        xguide = L"z",
+		yguide=L"\PP{\mu \geq 0 \mid Z=z}", size=(380,280))
 	plot!(lfsr_scalemix_plot, ts, lfsr_ci_kde_scalemix,
 		label="KDE-F-Loc", fillcolor=:darkorange, fillalpha=0.5, ylim=(0,1))
 	plot!(lfsr_scalemix_plot, ts, lfsr_ci_dkw_scalemix,
 		label="DKW-F-Loc",show_ribbon=false, alpha=0.9, color=:black)
-	plot!(lfsr_scalemix_plot, ts, lfsr_ci_lam_scalemix,
-		label="Amari",show_ribbon=true, fillcolor=:blue, fillalpha=0.4)
-	plot!(lfsr_scalemix_plot, xlabel = L"z",
-		ylabel=L"\PP{\mu \geq 0 \mid Z=z}", size=(380,280))
+	plot!(lfsr_scalemix_plot, ts, lfsr_ci_amari_scalemix,
+		label="AMARI",show_ribbon=true, fillcolor=:blue, fillalpha=0.4)
 end
 
 
 # ╔═╡ f7a59c42-4bfa-11eb-27df-cf611213fefc
-savefig(lfsr_scalemix_plot, "prostate_scalemix_lfsr.tikz")
+# savefig(lfsr_scalemix_plot, "prostate_scalemix_lfsr.tikz")
 
 
 # ╔═╡ Cell order:
