c981050508645ea3e72edc8d1fe42a59af493bf7
Nikos Ignatiadis <nikos.ignatiadis01@gmail.com>
2021-08-31 02:01:53 -0700

    juliaformatter on floc kde

--- src/flocalization_kde.jl
+++ src/flocalization_kde.jl
@@ -1,7 +1,7 @@
 abstract type InfiniteOrderKernel <: ContinuousUnivariateDistribution end
 
 function Base.show(io::IO, d::InfiniteOrderKernel)
-    print(io,   nameof(typeof(d)))
+    print(io, nameof(typeof(d)))
     print(io, " | bandwidth = ")
     print(io, d.h)
 end
@@ -22,27 +22,32 @@ K^*_{\\text{sinc}}(t) = \\mathbf 1( t \\in [-1,1])
 ```
 """
 struct SincKernel{H} <: InfiniteOrderKernel
-   h::H
+    h::H
 end
 SincKernel() = SincKernel(DataBasedDefault())
 
-function Empirikos._set_defaults(kernel::InfiniteOrderKernel, Zs::AbstractVector{<:Empirikos.AbstractNormalSample}; kwargs...)
-    Empirikos.skedasticity(Zs) == Empirikos.Homoskedastic() || throw("Only implemented for Homoskedastic Gaussian data.")
+function Empirikos._set_defaults(
+    kernel::InfiniteOrderKernel,
+    Zs::AbstractVector{<:Empirikos.AbstractNormalSample};
+    kwargs...,
+)
+    Empirikos.skedasticity(Zs) == Empirikos.Homoskedastic() ||
+        throw("Only implemented for Homoskedastic Gaussian data.")
     σ = std(Zs[1])
     n = length(Zs)
-    h = σ/sqrt(log(n))
+    h = σ / sqrt(log(n))
     @set kernel.h = h
 end
 
 
-Distributions.cf(a::SincKernel, t) = one(Float64)*(-1/a.h <= t <= 1/a.h)
+Distributions.cf(a::SincKernel, t) = one(Float64) * (-1 / a.h <= t <= 1 / a.h)
 
 function Distributions.pdf(a::SincKernel, t::Real)
-   if t==zero(Float64)
-       return(one(Float64)/pi/a.h)
-   else
-       return(sin(t/a.h)/pi/t)
-   end
+    if t == zero(Float64)
+        return (one(Float64) / pi / a.h)
+    else
+        return (sin(t / a.h) / pi / t)
+    end
 end
 
 
@@ -65,27 +70,27 @@ K^*_V(t) = \\begin{cases}
 ```
 """
 struct DeLaValleePoussinKernel{H} <: InfiniteOrderKernel
-   h::H
+    h::H
 end
 
 DeLaValleePoussinKernel() = DeLaValleePoussinKernel(DataBasedDefault())
 
 function Distributions.cf(a::DeLaValleePoussinKernel, t)
-   if abs(t * a.h) <= 1
-       return(one(Float64))
-   elseif abs(t * a.h) <=  2
-       return(2*one(Float64) - abs(t * a.h))
-   else
-       return(zero(Float64))
-   end
+    if abs(t * a.h) <= 1
+        return (one(Float64))
+    elseif abs(t * a.h) <= 2
+        return (2 * one(Float64) - abs(t * a.h))
+    else
+        return (zero(Float64))
+    end
 end
 
 function Distributions.pdf(a::DeLaValleePoussinKernel, t::Real)
-   if t==zero(Float64)
-       return(3*one(Float64)/2/pi/a.h)
-   else
-       return(a.h*(cos(t/a.h)-cos(2*t/a.h))/pi/t^2)
-   end
+    if t == zero(Float64)
+        return (3 * one(Float64) / 2 / pi / a.h)
+    else
+        return (a.h * (cos(t / a.h) - cos(2 * t / a.h)) / pi / t^2)
+    end
 end
 
 
@@ -117,27 +122,27 @@ struct FlatTopKernel{H} <: InfiniteOrderKernel
     h::H
 end
 
- FlatTopKernel() = FlatTopKernel(DataBasedDefault())
+FlatTopKernel() = FlatTopKernel(DataBasedDefault())
 
 function Distributions.cf(a::FlatTopKernel, t)
     if abs(t * a.h) <= 1
-        return(one(Float64))
-    elseif abs(t * a.h) <=  1.1
-        return(one(Float64) - 10*(abs(t * a.h) - one(Float64)))
+        return (one(Float64))
+    elseif abs(t * a.h) <= 1.1
+        return (one(Float64) - 10 * (abs(t * a.h) - one(Float64)))
     else
-        return(zero(Float64))
+        return (zero(Float64))
     end
- end
+end
 
- # TODO: This is *not* numerically stable.
- # But presumably do not need it to be (since the KDE code just calls cf)
+# TODO: This is *not* numerically stable.
+# But presumably do not need it to be (since the KDE code just calls cf)
 function Distributions.pdf(a::FlatTopKernel, t::Real)
     h = a.h
-    th = t/h
-    if t==zero(Float64)
-        return((0.55^2 - 0.5^2)*20/pi/h)
+    th = t / h
+    if t == zero(Float64)
+        return ((0.55^2 - 0.5^2) * 20 / pi / h)
     else
-        return((abs2(sin(0.55*th)) - abs2(sin(0.5*th)))/pi/th^2 * 20 / h)
+        return ((abs2(sin(0.55 * th)) - abs2(sin(0.5 * th))) / pi / th^2 * 20 / h)
     end
 end
 
@@ -165,24 +170,27 @@ Note that the bound is valid from `a_min` to `a_max`. `α` is the nominal level 
 `rng` sets the seed for the bootstrap samples.
 """
 Base.@kwdef struct InfinityNormDensityBand <: FLocalization
-   a_min = DataBasedDefault()
-   a_max = DataBasedDefault()
-   npoints::Integer = 1024
-   kernel = FlatTopKernel()
-   bootstrap = :Multinomial
-   nboot::Integer = 1000
-   α::Float64 = 0.05
-   rng = Random.MersenneTwister(1)
+    a_min = DataBasedDefault()
+    a_max = DataBasedDefault()
+    npoints::Integer = 1024
+    kernel = FlatTopKernel()
+    bootstrap = :Multinomial
+    nboot::Integer = 1000
+    α::Float64 = 0.05
+    rng = Random.MersenneTwister(1)
 end
 
 Empirikos.vexity(::InfinityNormDensityBand) = Empirikos.LinearVexity()
-function Empirikos._set_defaults(method::InfinityNormDensityBand,
-                                Zs::AbstractVector{<:Empirikos.AbstractNormalSample{<:Number}};
-                                hints...)
-    Empirikos.skedasticity(Zs) == Empirikos.Homoskedastic() || throw("Only implemented for Homoskedastic Gaussian data.")
+function Empirikos._set_defaults(
+    method::InfinityNormDensityBand,
+    Zs::AbstractVector{<:Empirikos.AbstractNormalSample{<:Number}};
+    hints...,
+)
+    Empirikos.skedasticity(Zs) == Empirikos.Homoskedastic() ||
+        throw("Only implemented for Homoskedastic Gaussian data.")
 
     q = get(hints, :quantile, 0.1)
-    a_min, a_max = quantile(response.(Zs),  (q, 1-q))
+    a_min, a_max = quantile(response.(Zs), (q, 1 - q))
     if isa(method.a_min, DataBasedDefault)
         method = @set method.a_min = a_min
     end
@@ -207,15 +215,15 @@ distributed according to a density ``f``..
 * `C∞`: The half-width of the L∞ band.
 * `fitted_kde`: The fitted `KernelDensity` object.
 """
-Base.@kwdef struct FittedInfinityNormDensityBand{T<:Real, S, K} <: FittedFLocalization
+Base.@kwdef struct FittedInfinityNormDensityBand{T<:Real,S,K} <: FittedFLocalization
     C∞::T
     a_min::T
     a_max::T
-    fitted_kde
-    interp_kde
+    fitted_kde::Any
+    interp_kde::Any
     midpoints::S
     estimated_density::K
-    boot_samples
+    boot_samples::Any
     method = nothing
 end
 
@@ -225,9 +233,13 @@ function Empirikos.nominal_alpha(inftyband::FittedInfinityNormDensityBand)
     Empirikos.nominal_alpha(inftyband.method)
 end
 
-function StatsBase.fit(opt::InfinityNormDensityBand,
-                       Zs::AbstractVector{<:Empirikos.AbstractNormalSample{<:Number}}; kwargs...)
-    Empirikos.skedasticity(Zs) == Empirikos.Homoskedastic() || throw("Only implemented for Homoskedastic Gaussian data.")
+function StatsBase.fit(
+    opt::InfinityNormDensityBand,
+    Zs::AbstractVector{<:Empirikos.AbstractNormalSample{<:Number}};
+    kwargs...,
+)
+    Empirikos.skedasticity(Zs) == Empirikos.Homoskedastic() ||
+        throw("Only implemented for Homoskedastic Gaussian data.")
     opt = Empirikos.set_defaults(opt, Zs; kwargs...)
 
     @unpack a_min, a_max, npoints, kernel, nboot, α, bootstrap, rng = opt
@@ -235,101 +247,112 @@ function StatsBase.fit(opt::InfinityNormDensityBand,
     # deepcopying below to make sure RNG status for sampling here remains the same.
     rng = deepcopy(rng)
 
-    res = certainty_banded_KDE(response.(Zs), a_min, a_max;
-                         npoints = npoints,
-                         rng = rng,
-                         kernel = kernel,
-                         bootstrap = bootstrap,
-                         nboot = nboot, α=α)
+    res = certainty_banded_KDE(
+        response.(Zs),
+        a_min,
+        a_max;
+        npoints = npoints,
+        rng = rng,
+        kernel = kernel,
+        bootstrap = bootstrap,
+        nboot = nboot,
+        α = α,
+    )
     res = @set res.method = opt
     Z = Zs[1]
-    normal_midpoints =  [@set Z.Z = mdpt for mdpt in res.midpoints]
+    normal_midpoints = [@set Z.Z = mdpt for mdpt in res.midpoints]
     res = @set res.midpoints = normal_midpoints
     res
 end
 
-function certainty_banded_KDE(Xs, a_min, a_max;
-                        kernel,
-                        rng = Random._GLOBAL_RNG,
-                        npoints = 4096,
-                        nboot = 1_000,
-                        bootstrap = :Multinomial,
-                        α=0.5)
+function certainty_banded_KDE(
+    Xs,
+    a_min,
+    a_max;
+    kernel,
+    rng = Random._GLOBAL_RNG,
+    npoints = 4096,
+    nboot = 1_000,
+    bootstrap = :Multinomial,
+    α = 0.5,
+)
 
     h = kernel.h
     m = length(Xs)
 
     lo, hi = extrema(Xs)
     # avoid FFT wrap-around numerical difficulties
-    lo_kde, hi_kde = min(a_min, lo - 6*h), max(a_max, hi + 6*h)
+    lo_kde, hi_kde = min(a_min, lo - 6 * h), max(a_max, hi + 6 * h)
     midpts = range(lo_kde, hi_kde; length = npoints)
 
     fitted_kde = kde(Xs, KernelDensity.UniformWeights(m), midpts, kernel)
     interp_kde = InterpKDE(fitted_kde)
 
     # quantities only at points of interest
-    midpts_idx = findall( (midpts .>= a_min) .& (midpts .<= a_max) )
+    midpts_idx = findall((midpts .>= a_min) .& (midpts .<= a_max))
     midpoints = fitted_kde.x[midpts_idx]
     estimated_density = fitted_kde.density[midpts_idx]
 
     C∞_boot = Vector{Float64}(undef, nboot)
 
     if bootstrap === :Multinomial
-        multi = Multinomial(m, fill(1/m, m))
+        multi = Multinomial(m, fill(1 / m, m))
     end
-    for k =1:nboot
+    for k = 1:nboot
         if bootstrap === :Poisson # Poisson bootstrap to estimate certainty band
             Z_pois = rand(rng, Poisson(1), m)
-            ws =  Weights(Z_pois/sum(Z_pois))
+            ws = Weights(Z_pois / sum(Z_pois))
         elseif bootstrap === :Multinomial # Efron bootstrap
             ws = Weights(vec(rand(rng, multi, 1)))
         else
             throw(error("Only :Multinomial and :Poisson supported."))
         end
-        f_kde_pois =  kde(Xs, ws, midpts, kernel)
+        f_kde_pois = kde(Xs, ws, midpts, kernel)
         C∞_boot[k] = maximum(abs.(estimated_density .- f_kde_pois.density[midpts_idx]))
     end
 
-    C∞ = quantile(C∞_boot, 1-α)
+    C∞ = quantile(C∞_boot, 1 - α)
 
-    FittedInfinityNormDensityBand(C∞=C∞,
-                    a_min=a_min,
-                    a_max=a_max,
-                    fitted_kde=fitted_kde,
-                    interp_kde=interp_kde,
-                    midpoints = midpoints,
-                    estimated_density = estimated_density,
-                    boot_samples = C∞_boot)
+    FittedInfinityNormDensityBand(
+        C∞ = C∞,
+        a_min = a_min,
+        a_max = a_max,
+        fitted_kde = fitted_kde,
+        interp_kde = interp_kde,
+        midpoints = midpoints,
+        estimated_density = estimated_density,
+        boot_samples = C∞_boot,
+    )
 end
 
 
-@recipe function f(ctband::FittedInfinityNormDensityBand; subsample=300)
+@recipe function f(ctband::FittedInfinityNormDensityBand; subsample = 300)
     y_all = ctband.estimated_density
     x_all = response.(ctband.midpoints)
 
     n = length(y_all)
-    _step = div(n-2, subsample)
+    _step = div(n - 2, subsample)
     idxs = [1; 2:_step:(n-1); n]
     x = x_all[idxs]
     y = y_all[idxs]
 
 
     xlims --> (a_min, a_max)
-    ylims --> (0, maximum(y)*1.1)
+    ylims --> (0, maximum(y) * 1.1)
     yguide --> "Density"
 
     α = nominal_alpha(ctband)
-    ci_level = 100*(1-α)
+    ci_level = 100 * (1 - α)
 
-	cis_ribbon  = ctband.C∞
-	fillalpha --> 0.36
-	seriescolor --> "#018AC4"
-	ribbon --> cis_ribbon
+    cis_ribbon = ctband.C∞
+    fillalpha --> 0.36
+    seriescolor --> "#018AC4"
+    ribbon --> cis_ribbon
     framestyle --> :box
     legend --> :topleft
     label --> "$ci_level% CI"
 
-	x, y
+    x, y
 end
 
 
@@ -337,7 +360,8 @@ end
 function Empirikos.flocalization_constraint!(
     model,
     ctband::FittedInfinityNormDensityBand,
-    prior::Empirikos.PriorVariable)
+    prior::Empirikos.PriorVariable,
+)
 
 
     C∞ = ctband.C∞
