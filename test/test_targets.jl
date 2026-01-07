06e2898390781a28b897583107e55fe614194155
Yihan (Hunter) Chen <123597242+huNterrchen@users.noreply.github.com>
2026-01-07 01:31:39 -0600

    Updated test cases for SignAgreementProbability with normal sample

--- test/test_targets.jl
+++ test/test_targets.jl
@@ -123,14 +123,28 @@ function signagree_closed_form(z, μ, τ)
     den = fz + fneg
     return num / den
 end
+
+function signagree_closed_form_normalsample(z, μ, τ)
+    σ = sqrt(1 + τ^2)
+    s = τ / σ
+    m = (τ^2 * z + μ) / (1 + τ^2)
+
+    if z > 0
+        return cdf(Normal(),  m / s)    
+    elseif z < 0
+        return cdf(Normal(), -m / s)   
+    else
+        return 0.5
+    end
+end
 @testset "SignAgreementProbability" begin
-    #test for numerator functionality
+    #test for numerator functionality (folded normal)
     t   = Empirikos.SignAgreementProbability(FoldedNormalSample(4))
     t_num = numerator(t)
     p = Normal(1,2)
     @test t_num(p) == numerator(Empirikos.PosteriorProbability(StandardNormalSample(4), Interval(0.0, Inf)))(p) +
                      numerator(Empirikos.PosteriorProbability(StandardNormalSample(-4), Interval(-Inf, 0.0)))(p)
-    #test for normal prior
+    #test for normal prior (folded normal)
     for (μ, τ) in ((0.0, 0.5), (0.0, 1.0), (0.4, 0.7), (-0.8, 1.2), (1.0, 2.0))
         prior = Normal(μ, τ)
         for z in (0.0, 0.25, 0.5, 1.0, 2.0, 3.0)
@@ -144,7 +158,7 @@ end
             @test isapprox(val_impl, val_exact; rtol=1e-11, atol=1e-12)
         end
     end
-    #test for uniform prior
+    #test for uniform prior (folded normal)
     prior = Uniform(-10.0, 10.0)
     t   = Empirikos.SignAgreementProbability(FoldedNormalSample(4))
             
@@ -153,5 +167,51 @@ end
     num_neg, _ = quadgk(μ -> (μ < 0) * pdf(Normal(μ, 1), -4) * pdf(prior, μ), -10, 10)
     
     @test isapprox(numerator(t)(prior), num_pos + num_neg, atol=1e-6)
+
+    #test for numerator functionality (normal sample)
+    prior = Normal(1, 2)
+    Zp = NormalSample(4.0, 1)
+    tp = Empirikos.SignAgreementProbability(Zp)
+    @test numerator(tp)(prior) ==
+          numerator(Empirikos.PosteriorProbability(Zp, Interval(0.0, Inf)))(prior)
+
+    Zn = NormalSample(-4.0, 1)
+    tn = Empirikos.SignAgreementProbability(Zn)
+    @test numerator(tn)(prior) ==
+          numerator(Empirikos.PosteriorProbability(Zn, Interval(-Inf, 0.0)))(prior)
+
+    #test for Normal prior (Normal sample)
+    for (μ, τ) in ((0.0, 0.5), (0.0, 1.0), (0.4, 0.7), (-0.8, 1.2), (1.0, 2.0))
+        prior = Normal(μ, τ)
+        for z in (-3.0, -1.0, -0.25, 0.0, 0.25, 1.0, 3.0)
+            t = Empirikos.SignAgreementProbability(NormalSample(z, 1))
+            num = numerator(t)(prior)
+            den = denominator(t)(prior)
+            val  = t(prior)
+            val_impl  = num / den
+            if z != 0.0
+                @test isapprox(val, val_impl; rtol=1e-11, atol=1e-12)
+            else
+                @test isapprox(val, 0.5; rtol=1e-11, atol=1e-12)
+            end
+            val_exact = signagree_closed_form_normalsample(z, μ, τ)
+            @test isapprox(t(prior), val_exact; rtol=1e-11, atol=1e-12)
+        end
+    end
+
+    #test for uniform prior (Normal sample)
+    prior = Uniform(-10.0, 10.0)
+
+    for z in (4.0, -4.0)
+        t = Empirikos.SignAgreementProbability(NormalSample(z, 1))
+
+        if z > 0
+            num_quad, _ = quadgk(μ -> (μ > 0) * pdf(Normal(μ, 1), z) * pdf(prior, μ), -10, 10)
+        else
+            num_quad, _ = quadgk(μ -> (μ < 0) * pdf(Normal(μ, 1), z) * pdf(prior, μ), -10, 10)
+        end
+
+        @test isapprox(numerator(t)(prior), num_quad; atol=1e-6)
+    end
 
 end
